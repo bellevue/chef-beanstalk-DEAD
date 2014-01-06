4b1828dd5bec9c8b6ceb48a5adb63951f8423a09
Jens Ulrich Hjuler Pedersen <multiply@juhp.net>
2014-01-06 09:51:12 +0100

    Now syncs the stuff correctly and restarts beanstalk

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,3 +7,24 @@ end
 
 # 1- Install
 include_recipe "beanstalk::#{node['beanstalk']['install_type']}"
+
+case node['platform']
+    when 'centos'
+        include_recipe 'yum::epel'
+        package_name = 'beanstalkd'
+    when 'ubuntu'
+        package_name = 'beanstalkd'
+end
+
+if node['platform'] == 'ubuntu'
+    template "/etc/default/beanstalkd" do
+        source "beanstalkd.erb"
+        mode 0644
+        owner "root"
+        group "root"
+    end
+end
+
+service service_name do
+    action :restart
+end
--- recipes/package.rb
+++ recipes/package.rb
@@ -2,23 +2,8 @@ case node['platform']
     when 'centos'
         include_recipe 'yum::epel'
         package_name = 'beanstalkd'
-        service_name = 'beanstalkd'
     when 'ubuntu'
         package_name = 'beanstalkd'
-        service_name = 'beanstalkd'
 end
 
 package package_name
-
-if node['platform'] == 'ubuntu'
-    template "/etc/default/beanstalkd" do
-        source "beanstalkd.erb"
-        mode 0644
-        owner "root"
-        group "root"
-    end
-end
-
-service service_name do
-    action :restart
-end
